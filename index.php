69ee3a07885905d6a8d78a719e7f334c037c79cb
Bastian Immel <bastian.immel@gmail.com>
2017-01-14 13:22:39 +0100

    added material

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
  <meta http-equiv="refresh" content="10; URL=http://localhost">
  <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
  <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
   <body>
     <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=50.3226&lon=-8.2469&name=BadCamberg&units=ca"> </iframe>
