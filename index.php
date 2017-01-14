9ae205dda7b1d4b570616f68a2cebfbbfa635a75
Bastian Immel <bastian.immel@gmail.com>
2017-01-14 12:52:25 +0100

    To PHP

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta http-equiv="refresh" content="10; URL=http://localhost">
+ <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <link rel="stylesheet" href="css/style.css">
+</head>
+  <body>
+    <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=50.3226&lon=-8.2469&name=BadCamberg&units=ca"> </iframe>
+  </body>
+</html>
