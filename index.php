47d4c85ffab218a80bf38f7d994128d1caca274a
Bastian Immel <bastian.immel@gmail.com>
2017-01-14 14:01:05 +0100

    SETUP

--- index.php
+++ index.php
@@ -108,9 +108,18 @@
         </div>
         <footer class="mdl-mini-footer">
         	<?php
-        		$host= gethostname(); 
-        		$ip = gethostbyname($host);
-        		echo $ip;
+				function getLocalIP(){
+				    exec("ipconfig /all", $output);
+				        foreach($output as $line){
+				            if (preg_match("/(.*)IPv4 Address(.*)/", $line)){
+				                $ip = $line;
+				                $ip = str_replace("IPv4 Address. . . . . . . . . . . :","",$ip);
+				                $ip = str_replace("(Preferred)","",$ip);
+				            }
+				        }
+				    return $ip;
+				}
+				echo $ip = getLocalIP();
         	?>
         </footer>
       </main>
