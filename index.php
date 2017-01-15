69b6fbe4ad02291a62dfd17a9ba07a9aff4505c4
Bastian Immel <bastian.immel@gmail.com>
2017-01-15 21:12:02 +0100

    SETUP

--- index.php
+++ index.php
@@ -192,7 +192,5 @@ $ip = getLocalIP();
           </g>
         </defs>
       </svg>
-      <a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/dashboard/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color-text--white">View Source</a>
-    <script src="$$hosted_libs_prefix$$/$$version$$/material.min.js"></script>
   </body>
 </html>
